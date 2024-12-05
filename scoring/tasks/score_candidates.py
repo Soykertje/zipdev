adcce6374fb316a5243262f20104a5fe2b8e5489
Soykertje <juanandresmolina16@gmail.com>
2024-12-04 23:55:57 -0500

    CHANGE: Modified task to use runpod endpoint.

--- scoring/tasks/score_candidates.py
+++ scoring/tasks/score_candidates.py
@@ -6,7 +6,8 @@ from django.conf import settings
 from scoring.models import ScoringTask, Candidate
 
 # Utils
-from utils.scoring.score_candidate import CandidateRanker
+import runpod
+import time
 from utils.scoring.helpers import extract_years_of_experience, map_education_level
 
 # Celery
@@ -16,22 +17,30 @@ from zipdev.celery import app
 @app.task(bind=True, ignore_result=True, max_retries=5, default_retry_delay=1, queue="score_dandidates")
 def score_candidates(self, scoring_task_id: int):
     scoring_task = ScoringTask.objects.get(id=scoring_task_id)
-    try:
-        scoring_task.status = ScoringTask.StatusChoices.PENDING
-        scoring_task.save()
-        candidates = list(Candidate.objects.values("id", "experiences", "summary", "skills", "educations"))
-        for candidate in candidates:
-            candidate["experience_years"] = extract_years_of_experience(candidate["experiences"])
-            candidate["education_score"] = map_education_level(candidate["experiences"])
-
-        ranker = CandidateRanker()
-        result = ranker.rank_candidates(candidates, scoring_task.query, settings.CANDIDATE_SCORING_WEIGHTS)
-        if len(result) > 30:
-            result = result[:30]
-        result = [{"id": element[0]["id"], "score": round(float(element[1]), 2) * 100} for element in result]
-        scoring_task.result = result
-    except Exception as e:
-        self.retry(exc=e)
-        return
+    candidates = list(Candidate.objects.values("id", "experiences", "summary", "skills", "educations"))
+    for candidate in candidates:
+        candidate["experience_years"] = extract_years_of_experience(candidate["experiences"])
+        candidate["education_score"] = map_education_level(candidate["experiences"])
+    runpod_api_key = settings.RUNPOD_API_KEY
+    runpod.api_key = runpod_api_key
+    endpoint_id = settings.SCORER_ENDPOINT_ID
+    endpoint = runpod.Endpoint(endpoint_id)
+    payload = {
+        "input": {
+            "query": scoring_task.query,
+            "candidates": candidates,
+            "key_weights": settings.CANDIDATE_SCORING_WEIGHTS,
+        }
+    }
+    response = endpoint.run(payload)
+    while response.status() in ["IN_QUEUE", "IN_PROGRESS"]:
+        time.sleep(1)
+    if response.status() != "COMPLETED":
+        self.retry()
+    response = response.output()
+    if len(response) > 30:
+        result = response[:30]
+    result = [{"id": element[0]["id"], "score": round(float(element[1]), 2) * 100} for element in result]
+    scoring_task.result = result
     scoring_task.status = ScoringTask.StatusChoices.COMPLETED
     scoring_task.save()
